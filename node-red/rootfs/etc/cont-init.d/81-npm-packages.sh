69174963d002aece1ced05fd41c9fd9127923b70
Franck Nijhof <frenck@geekchimp.com>
2018-06-02 00:47:09 +0200

    :rocket: Improves installation of additional NPM packages

--- node-red/rootfs/etc/cont-init.d/81-npm-packages.sh
+++ node-red/rootfs/etc/cont-init.d/81-npm-packages.sh
@@ -7,13 +7,13 @@
 source /usr/lib/hassio-addons/base.sh
 
 if hass.config.has_value 'npm_packages'; then
-    apk update \
-        || hass.die 'Failed updating Alpine packages repository indexes'
-
     cd /opt || hass.die "Could not change directory to Node-RED"
 
     for package in $(hass.config.get 'npm_packages'); do
-        npm install "$package" \
-            || hass.die "Failed installing npm package ${package}"
+        npm install \
+            --no-optional \
+            --only=production \
+            "$package" \
+                || hass.die "Failed installing npm package ${package}"
     done
 fi
