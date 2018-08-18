1acf4a6bbce8e600c797b2398381a48a774d28e2
Jorim Tielemans <tielemans.jorim@gmail.com>
2018-08-18 21:13:19 +0200

    Implement functions to get options and secret if possible

--- node-red/rootfs/etc/node-red/config.js
+++ node-red/rootfs/etc/node-red/config.js
@@ -2,6 +2,112 @@ const config = require('/config/node-red/settings.js');
 const fs = require('fs');
 const options = JSON.parse(fs.readFileSync('/data/options.json', 'utf8'));
 const bcrypt = require('bcryptjs');
+const yaml = require('js-yaml');
+
+/**
+ * Checks if a configuration option exists in the config file and has an actual value
+ *
+ * @param {string} key
+ *   Key of the config option
+ *
+ * @return
+ *   true if the key is a valid option, false otherwise.
+ */
+function is_option(key) {
+    if (!(key in options)) {
+        console.log('Key is not in options.json');
+        return false
+    }
+    if (!options[key]) {
+        console.log('Value of key is empty')
+        return false
+    }
+    return true
+}
+
+/**
+ * Fetches a configuration value from the add-on config file
+ *
+ * @param {string} key
+ *   Key of the config option
+ * @return
+ *   Value of the key in the configuration file
+ */
+function get_option(key) {
+    if (! is_option(key)) {
+        console.log('Key is not a valid option')
+        return false
+    }
+    if (is_secret(key)) {
+        return get_secret(key)
+    }
+    return options[key]
+}
+
+/**
+ * Checks if a configuration option is refering to a secret
+ *
+ * @param {string} key
+ *   Key of the config option
+ *
+ * @return
+ *   true if the key is a secret, false otherwise.
+ */
+function is_secret(key) {
+    if (!(key in options)) {
+        console.log('Key is not in options.json');
+        return false
+    }
+    value = options[key]
+
+    if(value.startsWith('!secret ')) {
+        console.log('Key is a secret');
+        return true
+    } else {
+        console.log('Key is not a secret or empty');
+        return false
+    }
+}
+
+/**
+ * Gets a configuration option value by getting it from secrets.yaml
+ *
+ * @param {string} key
+ *   Key of the config option
+ *
+ * @return
+ *   Value of the key in the referenced to the secrets file
+ */
+function get_secret(key) {
+    if (!fs.existsSync('/config')) {
+        console.log('This add-on does not support secrets!');
+        return false
+    }
+    if (!fs.existsSync('/config/secrets.yaml')) {
+        console.log('A secret was requested, but could not find a secrets.yaml!');
+        return false
+    }
+    if (!is_secret(key)) {
+        console.log('The requested secret does not reference the secrets.yaml');
+        return false
+    }
+
+    secret = options[key].substring(8)
+    secrets = yaml.load(fs.readFileSync('/config/secrets.yaml', 'utf8'));
+
+    if (!(secret in secrets)) {
+        console.log('Secret is not in the secrets.yaml file');
+        return false
+    }
+    value = secrets[secret]
+
+    if (!value) {
+        console.log('Value of secret is empty')
+        return false
+    }
+    console.log('Key is a secret')
+    return value;
+}
 
 // Sane and required defaults for the add-on
 config.uiHost = '0.0.0.0';
