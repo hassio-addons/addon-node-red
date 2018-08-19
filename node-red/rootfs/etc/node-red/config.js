95feee7082b18826b76db3afd9df40db8881942f
Jorim Tielemans <tielemans.jorim@gmail.com>
2018-08-19 17:01:38 +0200

    :ambulance: Fixes the use of secrets (#21)
    * Npm install js-yaml
    
    * Implement functions to get options and secret if possible
    
    * Using subkeys and implemented secrets for real this time
    
    And clarify that user permission doesn't support secrets
    
    * Empty values are still allowed
    
    At least when the doors are open
    
    * :camel: :snake: Let's do it Node.js style
    
    And use :camel: instead of :snake:
    
    * :tractor: Simplified secrets fetching
    
    * `Make sure to get value from options

--- node-red/rootfs/etc/node-red/config.js
+++ node-red/rootfs/etc/node-red/config.js
@@ -2,6 +2,46 @@ const config = require('/config/node-red/settings.js');
 const fs = require('fs');
 const options = JSON.parse(fs.readFileSync('/data/options.json', 'utf8'));
 const bcrypt = require('bcryptjs');
+const yaml = require('js-yaml');
+
+/**
+ * Gets a configuration option value by getting it from secrets.yaml
+ *
+ * @param {string} Value
+ *   Configuration value to resolve
+ *
+ * @return
+ *   Returns the converted configuration value
+ */
+function getSecret(value) {
+    if(!value.startsWith('!secret ')) {
+        // This is not a secret
+        return value
+    }
+
+    secret = value.substring(8)
+
+    if (!fs.existsSync('/config')) {
+        console.log('This add-on does not support secrets!');
+        process.exit(1);
+        return false
+    }
+    if (!fs.existsSync('/config/secrets.yaml')) {
+        console.log(`A secret ${secret} was requested, but could not find a secrets.yaml!`);
+        process.exit(1);
+        return false
+    }
+
+    secrets = yaml.load(fs.readFileSync('/config/secrets.yaml', 'utf8'));
+
+    if (!(secret in secrets)) {
+        console.log(`Secret ${secret} is not in the secrets.yaml file`);
+        process.exit(1);
+        return false
+    }
+
+    return secrets[secret];
+}
 
 // Sane and required defaults for the add-on
 config.uiHost = '0.0.0.0';
@@ -12,13 +52,13 @@ config.nodesDir = '/config/node-red/nodes';
 
 // Several settings
 config.uiPort = options.port;
-config.credentialSecret = options.credential_secret;
+config.credentialSecret = getSecret(options.credential_secret);
 
 // Set SSL if enabled
 if (options.ssl === true) {
     config.https = {
-        key: fs.readFileSync(`/ssl/${options.keyfile}`),
-        cert: fs.readFileSync(`/ssl/${options.certfile}`),
+        key: fs.readFileSync(`/ssl/${getSecret(options.keyfile)}`),
+        cert: fs.readFileSync(`/ssl/${getSecret(options.certfile)}`),
     };
     config.requireHttps = options.require_ssl;
 }
@@ -32,9 +72,9 @@ if (options.users.length !== 0) {
 
     options.users.forEach((user, index) => {
         config.adminAuth.users.push({
-            username: user.username,
-            password: bcrypt.hashSync(user.password),
-            permissions: user.permissions,
+            username: getSecret(user.username),
+            password: bcrypt.hashSync(getSecret(user.password)),
+            permissions: getSecret(user.permissions),
         });
     });
 }
@@ -42,16 +82,16 @@ if (options.users.length !== 0) {
 // Secure HTTP node
 if (options.http_node.username) {
     config.httpNodeAuth = {
-        user: options.http_node.username,
-        pass: bcrypt.hashSync(options.http_node.password),
+        user: getSecret(options.http_node.username),
+        pass: bcrypt.hashSync(getSecret(options.http_node.password)),
     };
 }
 
 // Secure static HTTP
 if (options.http_static.username) {
     config.httpStaticAuth = {
-        user: options.http_static.username,
-        pass: bcrypt.hashSync(options.http_static.password),
+        user: getSecret(options.http_static.username),
+        pass: bcrypt.hashSync(getSecret(options.http_static.password)),
     }
 }
 
