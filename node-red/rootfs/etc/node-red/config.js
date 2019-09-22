900a45e5c00c3b6ee0fecb754ce5a597664a73bc
Franck Nijhof <frenck@addons.community>
2019-09-22 16:09:18 +0200

    :fire: Removes secrets support, now natively supported by Hassio

--- node-red/rootfs/etc/node-red/config.js
+++ node-red/rootfs/etc/node-red/config.js
@@ -2,46 +2,6 @@ const config = require('/config/node-red/settings.js');
 const fs = require('fs');
 const options = JSON.parse(fs.readFileSync('/data/options.json', 'utf8'));
 const bcrypt = require('bcryptjs');
-const yaml = require('js-yaml');
-
-/**
- * Gets a configuration option value by getting it from secrets.yaml
- *
- * @param {string} Value
- *   Configuration value to resolve
- *
- * @return
- *   Returns the converted configuration value
- */
-function getSecret(value) {
-    if(!value.startsWith('!secret ')) {
-        // This is not a secret
-        return value
-    }
-
-    secret = value.substring(8)
-
-    if (!fs.existsSync('/config')) {
-        console.log('This add-on does not support secrets!');
-        process.exit(1);
-        return false
-    }
-    if (!fs.existsSync('/config/secrets.yaml')) {
-        console.log(`A secret ${secret} was requested, but could not find a secrets.yaml!`);
-        process.exit(1);
-        return false
-    }
-
-    secrets = yaml.load(fs.readFileSync('/config/secrets.yaml', 'utf8'));
-
-    if (!(secret in secrets)) {
-        console.log(`Secret ${secret} is not in the secrets.yaml file`);
-        process.exit(1);
-        return false
-    }
-
-    return secrets[secret];
-}
 
 // Set dark theme if enabled
 if (options.dark_mode) {
@@ -69,21 +29,21 @@ config.adminAuth = null;
 config.https = null;
 
 // Several settings
-config.credentialSecret = getSecret(options.credential_secret);
+config.credentialSecret = options.credential_secret;
 
 // Secure HTTP node
 if (options.http_node.username) {
     config.httpNodeAuth = {
-        user: getSecret(options.http_node.username),
-        pass: bcrypt.hashSync(getSecret(options.http_node.password)),
+        user: options.http_node.username,
+        pass: bcrypt.hashSync(options.http_node.password),
     };
 }
 
 // Secure static HTTP
 if (options.http_static.username) {
     config.httpStaticAuth = {
-        user: getSecret(options.http_static.username),
-        pass: bcrypt.hashSync(getSecret(options.http_static.password)),
+        user: options.http_static.username,
+        pass: bcrypt.hashSync(options.http_static.password),
     }
 }
 
