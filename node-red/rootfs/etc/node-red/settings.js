2bbc8e2043ea555f63e6852f4524cc8813e1f93a
Franck Nijhof <frenck@addons.community>
2021-08-11 09:00:35 +0200

    🔥 Clean up unused require_ssl setting (#1095)

--- node-red/rootfs/etc/node-red/settings.js
+++ node-red/rootfs/etc/node-red/settings.js
@@ -35,7 +35,6 @@
  * - logging.console.level (log_level in the add-on configuration)
  * - httpNodeAuth (http_node settings in the add-on configuration)
  * - httpStaticAuth (http_static settings in the add-on configuration)
- * - requireHttps (require_ssl setting in the add-on configuration)
  * - httpNodeRoot (set fixed to `/endpoint` )
  *
  * If you like to change those settings, some are available via the add-on
